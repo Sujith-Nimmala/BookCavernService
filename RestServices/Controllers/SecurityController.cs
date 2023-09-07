1249c2a1737ff8ee9c5adb4b867339e2b5c05455
tandursudhasri <tandursudhasri@gmail.com>
2023-09-06 22:27:59 -0700

    added controllers

---
+++ RestServices/Controllers/SecurityController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using REST.Config;
+
+namespace REST.Controllers
+{
+    [ApiController]
+    public class SecurityController : ControllerBase
+    {
+        IConfiguration config;
+        SecurityService service;
+        public SecurityController(IConfiguration _config, SecurityService service)
+        {
+            config = _config;
+            this.service = service;
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public IActionResult Login(AppUserCredentalsModel model)
+        {
+            TokenAndRole? tokenAndRole = service.AuthenticateUserAndGetToken(model);
+            if (tokenAndRole == null)
+            {
+                return BadRequest("Invalid UserName or Password..");
+            }
+            else
+                return Ok(tokenAndRole);
+        }
+    }
+}
